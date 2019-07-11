ee5af5f3684c4c7cb6c68050029e68a85d2ce2f8
subject026 <sudobuildspeakers@gmail.com>
2019-07-11 13:46:22 +0100

    work so far

---
+++ src/js/DOM.js
@@ -0,0 +1,19 @@
+export const sel = {
+  // nav toggle
+  navToggleEl: "[data-js=nav-toggle]",
+  navEl: "[data-js=nav]",
+  overlayEl: "[data-js=overlay]",
+  // invoice search
+  invoicesSearchForm: "[data-js=invoice-search-form]",
+  invoiceSearchInput: "[data-js=invoice-search-input]",
+  // invoice add button
+  invoiceAddButton: "[data-js=invoice-add-button]"
+};
+
+export const cls = {
+  // nav toggle
+  navHidden: "layout-nav--is-hidden",
+  overlayHidden: "layout-overlay--is-hidden",
+  // invoice search
+  searchFormVisible: "search-form--is-visible"
+};
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,55 @@
-const navToggleEl = document.querySelector(".header__nav-toggle");
-const navEl = document.querySelector(".nav");
+import * as DOM from "./DOM";
 
-navToggleEl.addEventListener("click", () => {
-  navEl.classList.toggle("nav--is-hidden");
-});
+function setInvoiceAddButtonText(btn, isMed) {
+  if (isMed) {
+    btn.textContent = "Add Invoice";
+  } else {
+    btn.textContent = "ADD";
+  }
+}
+
+window.onload = () => {
+  // toggle navigation on mobile
+  const navToggleEl = document.querySelector(DOM.sel.navToggleEl);
+  const navEl = document.querySelector(DOM.sel.navEl);
+  const overlayEl = document.querySelector(DOM.sel.overlayEl);
+
+  navToggleEl.addEventListener("click", () => {
+    navEl.classList.toggle(DOM.cls.navHidden);
+    overlayEl.classList.toggle(DOM.cls.overlayHidden);
+  });
+
+  // show / hide invoice search form
+  const invoicesSearchInputEl = document.querySelector(
+    DOM.sel.invoiceSearchInput
+  );
+  const invoicesSearchForm = document.querySelector(DOM.sel.invoicesSearchForm);
+
+  invoicesSearchInputEl.addEventListener("focus", () => {
+    if (!invoicesSearchForm.classList.contains(DOM.cls.searchFormVisible)) {
+      invoicesSearchForm.classList.add(DOM.cls.searchFormVisible);
+    }
+  });
+
+  invoicesSearchInputEl.addEventListener("blur", event => {
+    if (event.target.value < 1) {
+      invoicesSearchForm.classList.remove(DOM.cls.searchFormVisible);
+    }
+  });
+
+  // change add invoice button text depending on window size
+
+  const invoiceAddButtonEl = document.querySelector(DOM.sel.invoiceAddButton);
+
+  let windowIsMed = window.matchMedia("(min-width: 650px)").matches;
+  setInvoiceAddButtonText(invoiceAddButtonEl, windowIsMed);
+
+  window.onresize = () => {
+    // get window width
+    const matchNow = window.matchMedia("(min-width: 650px)").matches;
+    if (windowIsMed !== matchNow)
+      setInvoiceAddButtonText(invoiceAddButtonEl, matchNow);
+    windowIsMed = matchNow;
+    //
+  };
+};
